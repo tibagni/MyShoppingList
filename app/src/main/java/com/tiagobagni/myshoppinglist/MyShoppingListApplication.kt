6afb0f22d94e48b356291d69ffb45562424eb223
Tiago Bagni <tibagni@gmail.com>
2018-08-31 21:25:24 -0300

    Add AppCenter SDK

--- app/src/main/java/com/tiagobagni/myshoppinglist/MyShoppingListApplication.kt
+++ app/src/main/java/com/tiagobagni/myshoppinglist/MyShoppingListApplication.kt
@@ -3,10 +3,20 @@ package com.tiagobagni.myshoppinglist
 import android.app.Application
 import com.tiagobagni.myshoppinglist.di.modules
 import org.koin.android.ext.android.startKoin
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MyShoppingListApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin(this, modules)
+        AppCenter.start(
+            this,
+            BuildConfig.APP_CENTER_SECRET,
+            Analytics::class.java,
+            Crashes::class.java
+        )
     }
 }
